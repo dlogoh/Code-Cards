f6c559ff08fddf6353741ee95be9bb7a025031cf
David Harris <87347646+dlogoh@users.noreply.github.com>
2023-03-27 22:16:15 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <main className='App'>
+      <Navbar />
+    </main>
+  );
+}
+
+export default App;
