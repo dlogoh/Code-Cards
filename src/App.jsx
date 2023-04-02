bd58b20e2a1e666f956d71718c5efb99c26c4394
dlogoh <davidlharris0@gmail.com>
2023-04-01 22:06:12 -0400

    Added footer and some react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Footer from "./components/Footer";
 import "./styles/App.css";
 
 function App() {
   return (
-    <main className='App'>
-      <div className='nav-container'>
-        <Navbar />
-      </div>
-      <Home />
-    </main>
+    <>
+      <main className='App'>
+        <div className='nav-container'>
+          <Navbar />
+        </div>
+        <Home />
+      </main>
+      <Footer />
+    </>
   );
 }
 
