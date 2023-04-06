f0cdf426cf8f7b8a1db3e34c8106e99374890352
David Harris <87347646+dlogoh@users.noreply.github.com>
2023-04-05 23:05:43 -0400

    Created routes, models, and middleware

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
+require("dotenv").config();
 const express = require("express");
-const dotenv = require("dotenv").config();
 const port = process.env.PORT;
+const connectDB = require("../config/db");
 
 const app = express();
 
-app.get("/api/goals", (req, res) => {
-  res.send("Hello from get req");
-});
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => res.send("API Running..."));
+
+// Routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
